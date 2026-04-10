f0017f45d01c084dfeb447357200a5d86ebb0626
diegoefu <diegoafbrito12@gmail.com>
2026-04-10 12:01:26 -0400

    agregar modelos iniciales

--- files/models.py
+++ files/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+class Seccion(models.Model):
+    nombre = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+    
+class Carpeta(models.Model):
+    nombre = models.CharField(max_length=255)
+    carpeta = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='carpetas')
+    seccion = models.ForeignKey(Seccion, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Archivo(models.Model):
+    nombre = models.CharField(max_length=255)
+    direccion = models.FileField(upload_to='archivos/')
+    carpeta = models.ForeignKey(Carpeta, on_delete=models.CASCADE, related_name='archivos')
+    version_siguiente = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='versiones_anteriores')
+    estado = models.CharField(max_length=1, default='P') # 'P' Publicado, 'B' Borrador, 'E' Eliminado 
+
+    def __str__(self):
+        return self.name
+    
+class Registro(models.Model):
+    archivo = models.ForeignKey(Archivo, on_delete=models.CASCADE, related_name='registros', null=True, blank=True)
+    carpeta = models.ForeignKey(Carpeta, on_delete=models.CASCADE, related_name='registros', null=True, blank=True)
+    seccion = models.ForeignKey(Seccion, on_delete=models.CASCADE, related_name='registros', null=True, blank=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+    accion = models.CharField(max_length=1) # 'C' Creación, 'M' Modificación, 'E' Eliminación
+    usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    descripcion = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Cambio en {self.archivo.nombre} por {self.usuario} el {self.fecha}"
