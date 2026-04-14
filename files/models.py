48f5b1b8e12d4cb77b2c3eaa50b1b01b46aedaf9
diegoefu <diegoafbrito12@gmail.com>
2026-04-14 10:59:42 -0400

    añadir consulta de carpetas de seccion

--- files/models.py
+++ files/models.py
@@ -15,6 +15,12 @@ class Carpeta(models.Model):
     def __str__(self):
         return self.nombre
     
+    def fecha_de_carga(self):
+        return self.registros.get(accion='C').fecha if self.registros.filter(accion='C').exists() else None
+    
+    def creado_por(self):
+        return self.registros.get(accion='C').usuario if self.registros.filter(accion='C').exists() else None
+    
 class Archivo(models.Model):
     nombre = models.CharField(max_length=255)
     direccion = models.FileField(upload_to='archivos/')
@@ -25,6 +31,12 @@ class Archivo(models.Model):
     def __str__(self):
         return self.nombre
     
+    def fecha_de_carga(self):
+        return self.registros.get(accion='C').fecha if self.registros.filter(accion='C').exists() else None
+    
+    def creado_por(self):
+        return self.registros.get(accion='C').usuario if self.registros.filter(accion='C').exists() else None
+
 class Registro(models.Model):
     archivo = models.ForeignKey(Archivo, on_delete=models.CASCADE, related_name='registros', null=True, blank=True)
     carpeta = models.ForeignKey(Carpeta, on_delete=models.CASCADE, related_name='registros', null=True, blank=True)
