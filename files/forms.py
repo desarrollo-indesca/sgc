99e23421e42c8531ebcde1cde9f021a229981c7c
diegoefu <diegoafbrito12@gmail.com>
2026-04-21 13:04:25 -0400

    añadir arreglos en carga de archivo

--- files/forms.py
+++ files/forms.py
@@ -8,6 +8,24 @@ class CrearCarpetaForm(forms.ModelForm):
         fields = ['nombre']
 
 class CrearArchivoForm(forms.ModelForm):
+    mantener_nombre = forms.BooleanField(required=False)
+
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        mantener_nombre = self.cleaned_data.get('mantener_nombre')
+
+        if not mantener_nombre:
+            # Si no se mantiene el nombre, se puede modificar para evitar conflictos
+            return nombre
+        else:
+            # Si se mantiene el nombre, se debe verificar que no exista otro archivo con el mismo nombre en la sección
+            seccion = self.instance.seccion
+            if Archivo.objects.filter(nombre=nombre, seccion=seccion).exists():
+                raise forms.ValidationError("Ya existe un archivo con ese nombre en esta sección.")
+            elif Carpeta.objects.filter(nombre=nombre, carpeta=self.instance.carpeta).exists():
+                raise forms.ValidationError("Ya existe una carpeta con ese nombre en esta sección.")
+            return nombre
+
     class Meta:
         model = Archivo
         fields = ['nombre', 'direccion']
--- files/views.py
+++ files/views.py
@@ -66,14 +66,76 @@ class SeccionCarpetaListView(LoginRequiredMixin, View):
             'carpetas': Carpeta.objects.filter(seccion__pk=seccion, activo=True),
             'seccion': Seccion.objects.get(pk=seccion),
             'form_carpeta': CrearCarpetaForm(prefix='carpeta'),
+            'form_archivo': CrearArchivoForm(prefix='archivo'),
         })
     
     def post(self, request, seccion):
+            archivo = request.POST.get('archivo')
             nombre_carpeta = request.POST.get('carpeta-nombre')
             eliminar = request.POST.get('eliminar')
             editar = request.POST.get('editar')
 
             with transaction.atomic():
+                if(archivo):
+                    form_archivo = CrearArchivoForm(request.POST, prefix='archivo')
+                    
+                    try:
+                        if form_archivo.is_valid():
+                                form_archivo.instance.seccion = Seccion.objects.get(pk=seccion)
+                                form_archivo.instance.save()
+
+                                Registro.objects.create(
+                                    archivo = form_archivo.instance,
+                                    accion = "C",
+                                    usuario = request.user,
+                                    descripcion = f"Creación de la carpeta {form_carpeta.instance.nombre} en la sección {form_carpeta.instance.seccion}"
+                                )
+
+                                messages.success(request, 'Carpeta creada.')
+                                return redirect(f'/list/{seccion}/')
+                        else:
+                                messages.error(request, 'Error al crear la carpeta.')
+                                print(form_carpeta.errors)
+                                return redirect(f'/list/{seccion}/')
+                    except Exception as ex:
+                        messages.error(request, f'Error al crear la carpeta: {str(ex)}')
+                        return redirect(f'/list/{seccion}/')
+                elif(eliminar):
+                    carpeta = Carpeta.objects.get(pk=eliminar)
+                    carpeta.activo = False
+                    carpeta.save()
+
+                    Registro.objects.create(
+                        carpeta = carpeta,
+                        accion = "E",
+                        usuario = request.user,
+                        descripcion = f"Eliminación de la carpeta {carpeta.nombre} en la sección {carpeta.seccion}"
+                    )
+
+                    messages.success(request, 'Carpeta eliminada.')
+                    return redirect(f'/list/{seccion}/')
+                elif(editar):
+                    carpeta = Carpeta.objects.get(pk=editar)
+                    name_old = carpeta.nombre
+                    
+                    try:
+                            carpeta.nombre = request.POST.get('carpeta-nombre')
+                            carpeta.save()
+
+                            Registro.objects.create(
+                                carpeta = carpeta,
+                                accion = "U",
+                                usuario = request.user,
+                                descripcion = f"Edición de la carpeta {name_old} a {carpeta.nombre} en la sección {carpeta.seccion}"
+                            )              
+
+                            messages.success(request, 'Carpeta editada.')
+                            return redirect(f'/list/{seccion}/')
+                    except Exception as ex:
+                        messages.error(request, f'Error al crear la carpeta: {str(ex)}')
+                        return redirect(f'/list/{seccion}/')
+
+
                 if(nombre_carpeta and not editar):
                     form_carpeta = CrearCarpetaForm(request.POST, prefix='carpeta')
                     
